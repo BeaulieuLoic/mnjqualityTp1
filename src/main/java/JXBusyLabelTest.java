02e2130c493820b4b4d4e6b44adbc4aedc743ffa
BeaulieuLoic <votre_email@votre_email.com>
2017-09-12 16:43:55 +0200

    add project

---
+++ src/main/java/JXBusyLabelTest.java
@@ -0,0 +1,76 @@
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+import org.jdesktop.swingx.JXBusyLabel;
+
+
+/**
+ * Description de la class
+ */
+public class JXBusyLabelTest extends JFrame implements ActionListener {
+
+    private JXBusyLabel bLabel1;
+    private JComboBox comboBackground, comboForeground;
+    private JButton btnStart, btnStop;
+    private Color[] colors = {Color.BLACK, Color.WHITE, Color.RED, Color.BLUE, Color.GREEN, Color.GRAY, Color.YELLOW};
+
+    private JXBusyLabelTest() {
+        bLabel1 = new JXBusyLabel(new Dimension(60, 60));
+        bLabel1.setBusy(true);
+
+        if (false || false){
+            System.out.printf("aze");
+        }
+
+        String[] colorsName = {"Choose a color", "BLACK", "WHITE", "RED", "BLUE", "GREEN", "GRAY", "YELLOW"};
+        comboBackground = new JComboBox(colorsName);
+        comboForeground = new JComboBox(colorsName);
+        btnStart = new JButton("Start");
+        btnStop = new JButton("Stop");
+
+        comboBackground.addActionListener(this);
+        comboForeground.addActionListener(this);
+        btnStart.addActionListener(this);
+        btnStop.addActionListener(this);
+
+        JPanel panelNorth = new JPanel();
+        JPanel panelSouth = new JPanel();
+        panelSouth.add(new JLabel("Background"));
+        panelSouth.add(comboBackground);
+        panelSouth.add(new JLabel("Foreground"));
+        panelSouth.add(comboForeground);
+        panelNorth.add(btnStart);
+        panelNorth.add(btnStop);
+
+        add(panelNorth, BorderLayout.NORTH);
+        add(bLabel1, BorderLayout.CENTER);
+        add(panelSouth, BorderLayout.SOUTH);
+
+        setSize(400, 160);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == comboBackground) {
+            bLabel1.getBusyPainter().setBaseColor(colors[comboBackground.getSelectedIndex()-1]);
+        } else if (e.getSource() == comboForeground) {
+            bLabel1.getBusyPainter().setHighlightColor(colors[comboForeground.getSelectedIndex()-1]);
+        } else if (e.getSource() == btnStart) {
+            bLabel1.setBusy(true);
+        } else if (e.getSource() == btnStop) {
+            bLabel1.setBusy(false);
+        }
+    }
+
+    public static void main(String[] args) {
+        new JXBusyLabelTest();
+    }
+
+}
